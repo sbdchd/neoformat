b25e886c978e61cbbe8a8e0fbb8da1cfd193e5d9
Steve Dignam <steve@dignam.xyz>
2017-01-07 18:01:01 -0500

    Removed running of formatters in install.sh

--- test/install.sh
+++ test/install.sh
@@ -1,9 +1,4 @@
 npm install -g csscomb@3.1.7
-csscomb --version
 npm install -g prettydiff@99.0.1
-prettydiff --version
 npm install -g js-beautify@1.6.2 # for css-beautify
-css-beautify --version
 pip install yapf==0.14.0
-
-yapf --version
